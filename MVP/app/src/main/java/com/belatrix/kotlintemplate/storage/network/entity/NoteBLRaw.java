ac70a447b5f1885b709dfa0ea1dad1ee7ef964b8
Eduardo Medina <emedinaa@gmail.com>
2018-03-13 16:30:44 -0500

    Add MVP sample

---
+++ MVP/app/src/main/java/com/belatrix/kotlintemplate/storage/network/entity/NoteBLRaw.java
@@ -0,0 +1,27 @@
+package com.belatrix.kotlintemplate.storage.network.entity;
+
+/**
+ * Created by emedinaa on 17/11/17.
+ */
+
+public class NoteBLRaw {
+
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
