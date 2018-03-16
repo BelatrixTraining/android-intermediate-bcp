6d8320e38cc85fd6a27ebbc46716192ff30e5479
Eduardo Medina <emedinaa@gmail.com>
2018-03-16 13:47:54 -0500

    Add Camera sample

---
+++ Camera/app/src/main/java/com/belatrix/kotlintemplate/media/CameraActivity.java
@@ -0,0 +1,29 @@
+package com.belatrix.kotlintemplate.media;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.belatrix.kotlintemplate.R;
+import com.belatrix.kotlintemplate.fragments.OnCamerActionListener;
+
+
+public class CameraActivity extends AppCompatActivity implements OnCamerActionListener {
+
+    private final  int REQUEST_CAMERA= 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+    public void sendPhoto(String path) {
+        Bundle bundle= new Bundle();
+        bundle.putString("PATH",path);
+        Intent intent= new Intent();
+        intent.putExtras(bundle);
+
+        setResult(REQUEST_CAMERA,intent);
+        finish();
+    }
+}
